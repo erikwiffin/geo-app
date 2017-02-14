6934f38e99b80c2a3749ab50aaab2206ecb231e4
Erik Wiffin <erik.wiffin@gmail.com>
2017-02-14 05:55:17 +0000

    Make it look pretty

--- geo_app/filters.py
+++ geo_app/filters.py
@@ -1,9 +1,15 @@
 # pylint: disable=missing-docstring
+from datetime import datetime
 import json
 
 import jinja2
 
 from geo_app.application import app
+
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 
 @app.template_filter('jsonify')
